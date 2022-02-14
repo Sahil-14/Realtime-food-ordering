d32a197eb244fc5e859e2d5d47582c3ffe1218a4
sahil shimpi <sahilshimpi@Sahil.local>
2022-02-14 19:48:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require('ejs')
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//set template engine
+app.get("/", (req, res) => {
+  res.render("home")
+})
+
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT)
+})
